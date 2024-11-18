7c8967ea22c21e0a1a2055eb56417854bbf3aa6a
h4shBrowns <fohe24kc@student.ju.se>
2024-11-18 17:43:21 +0100

    Add booster flame and other attributes
    Added a booster flame underneath the spaceship. Also applied the gravity to the spaceship when no input is given as well as preventing spaceship from going through the ground.

--- game.js
+++ game.js
@@ -1,45 +1,72 @@
 // Initial position and gravity + booster for spaceship
 let spaceshipX = 400;
-let spaceshipY = 400;
-let velocity = 0;
+let spaceshipY = 150;
+let velocityY = 0;
 const gravity = 0.1;
-const bosstThrush = -3;
+const boostThrust = -3;
 
 // Setups and creates the canvas width / height
 function setup() {
   createCanvas(800, 600);
 }
 
-// Takes x and y positions and draws character "spaceship" on called coordinates
-function spaceship(x, y, thrust) {
+// Function takes x and y positions and draws character "spaceship" on called coordinates
+function spaceship(x, y, boostThrusting) {
+  
+  // Spaceship body
   stroke(150, 150, 150);
   fill(0, 255, 100);
   ellipse(x, y, 100, 150);
 
+  //Spaceship window
   fill(255, 255, 255);
   stroke(0, 0, 0);
   circle(x, y - 20, 50, 50);
 
+  //Spaceship legs
   stroke(150, 150, 150);
   fill(100, 100, 100);
   bezier(x - 35, y + 50, x - 5, y + 70, x - 15, y + 70, x - 60, y + 90);
   bezier(x + 35, y + 50, x + 5, y + 70, x + 15, y + 70, x + 60, y + 90);
 
+  //Spaceship tip
   triangle(x, y - 120, x - 30, y - 60, x + 30, y - 60);
 
+  //Spaceship passenger
   fill(0, 0, 0);
   strokeWeight(3);
   stroke(0, 0, 0);
   line(x, y + 5, x, y - 10);
   line(x, y + 5, x - 8, y - 5);
   line(x, y + 5, x + 8, y - 5);
-
   circle(x, y - 15, 10, 10);
+
+  if (boostThrusting) {
+    noStroke();
+    fill(255, 100, 0);
+    ellipse(x, y + 90, 30, 50);
+    fill(255, 200, 0);
+    ellipse(x, y + 90, 15, 30);
+  }
 }
 
+
+
 function draw() {
+
   background('black');
+
   fill('grey');
   rect(0, 490, 800, 400);
-  spaceship(400, 150);
+
+  velocityY += gravity;
+
+  spaceshipY += velocityY;
+  
+  if (spaceshipY > 440) {
+    spaceshipY = 440;
+    velocityY = 0; 
+  }
+
+  spaceship(spaceshipX, spaceshipY, boostThrust);
 }
